25f0f17f20cecd5f7b254f2ea215558833784e63
Shahar-Levy <office@monodon.net>
2018-08-02 17:29:38 +0300

    feat: add service worker caching

--- js/main.js
+++ js/main.js
@@ -215,4 +215,18 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
+
+/** Register service worker **/
+const registerServiceWorker = _ => {
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register("../serviceworker.js")
+      .then(registration => {
+        console.log("Service worker registered succesfully");
+      })
+      .catch(error => {
+        console.log('Registration failed', error);
+      })
+  }
+}
+registerServiceWorker()
 }
