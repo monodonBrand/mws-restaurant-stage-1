a82815be66c76d57a869ee7be0532ddc91597c4b
Shahar-Levy <office@monodon.net>
2018-08-07 17:52:29 +0300

    fix: fetch data from server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -6,29 +6,29 @@ class DBHelper {
   /**
    * Database URL.
    * Change this to restaurants.json file location on your server.
-   */
+
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
     return `http://localhost:${port}/data/restaurants.json`;
-  }
+  }*/
 
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch('http://localhost:1337/restaurants')
+    .then(response => {
+      return response.json()
+        .then(body => {
+          if(response.ok) {
+            const restaurants = body
+            callback(null, restaurants)
+            return body
+          }
+          return Promise.reject({body})
+        })
+    })
+    .catch(error => console.log(error))
   }
 
   /**
